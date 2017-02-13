4419a7c9089ede70a27e813b742f3e254977d529
Marin Atanasov <tyxla@abv.bg>
2017-02-13 12:49:37 +0100

    Login: Connect to redux store and add simple form submission

--- client/blocks/login/index.jsx
+++ client/blocks/login/index.jsx
@@ -2,15 +2,17 @@
  * External dependencies
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 //import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
  */
-import Button from 'components/button';
+import FormsButton from 'components/forms/form-button';
 import Card from 'components/card';
 import FormTextInput from 'components/forms/form-text-input';
 import { localize } from 'i18n-calypso';
+import { loginUser } from 'state/login/actions';
 
 class Login extends Component {
 	static defaultProps = {
@@ -19,35 +21,68 @@ class Login extends Component {
 		legalText: ''
 	};
 
+	state = {
+		usernameOrEmail: '',
+		password: '',
+	};
+
+	onChangeField = ( event ) => {
+		this.setState( {
+			[ event.target.name ]: event.target.value
+		} );
+	};
+
+	onSubmitForm = ( event ) => {
+		event.preventDefault();
+		this.props.loginUser( this.state.usernameOrEmail, this.state.password );
+	};
+
 	render() {
 		const buttonText = this.props.buttonText || this.props.translate( 'Sign in' );
 		return (
 			<div className="login">
-				<Card className="login__form-userdata">
-					<div className="login__form-header">
-						<div className="login__form-header-title">{ this.props.title }</div>
-					</div>
-					{ this.props.children }
-					<div className="login__form-userdata">
-						<label className="login__form-userdata-username">
-							{ this.props.translate( 'Username or email' ) }
-							<FormTextInput className="login__form-userdata-username-input" />
-						</label>
-						<label className="login__form-userdata-username">
-							{ this.props.translate( 'Password' ) }
-							<input className="login__form-userdata-username-password" type="password" />
-						</label>
-					</div>
-				</Card>
-				<Card className="login__form-action">
-					<div className="login__form-action-legal">
-						{ this.props.legalText }
-					</div>
-					<Button primary>{ buttonText }</Button>
-				</Card>
+				<form onSubmit={ this.onSubmitForm }>
+					<Card className="login__form-userdata">
+						<div className="login__form-header">
+							<div className="login__form-header-title">{ this.props.title }</div>
+						</div>
+						{ this.props.children }
+						<div className="login__form-userdata">
+							<label className="login__form-userdata-username">
+								{ this.props.translate( 'Username or email' ) }
+								<FormTextInput
+									className="login__form-userdata-username-input"
+									onChange={ this.onChangeField }
+									name="usernameOrEmail"
+									value={ this.state.usernameOrEmail }
+								/>
+							</label>
+							<label className="login__form-userdata-username">
+								{ this.props.translate( 'Password' ) }
+								<input
+									className="login__form-userdata-username-password"
+									onChange={ this.onChangeField }
+									type="password"
+									name="password"
+									value={ this.state.password }
+								/>
+							</label>
+						</div>
+					</Card>
+					<Card className="login__form-action">
+						<div className="login__form-action-legal">
+							{ this.props.legalText }
+						</div>
+						<FormsButton>
+							{ buttonText }
+						</FormsButton>
+					</Card>
+				</form>
 			</div>
 		);
 	}
 }
 
-export default localize( Login );
+export default connect( null, {
+	loginUser
+} )( localize( Login ) );
