31269ac9ce06cb68b4898a77ad3e6a752c2abf5b
Marin Atanasov <tyxla@abv.bg>
2017-02-15 11:04:18 +0100

    Login: Store two factor data in state

--- client/state/login/selectors.js
+++ client/state/login/selectors.js
@@ -9,3 +9,12 @@ export const isLoginSuccessful = ( state ) => {
 export const getError = ( state ) => {
 	return state.login.requestError;
 };
+
+export const isTwoFactorEnabled = ( state ) => {
+	return (
+		state.login.twoFactorAuth &&
+		state.login.twoFactorAuth.result &&
+		state.login.twoFactorAuth.twostep_id !== '' &&
+		state.login.twoFactorAuth.twostep_nonce !== ''
+	);
+};
